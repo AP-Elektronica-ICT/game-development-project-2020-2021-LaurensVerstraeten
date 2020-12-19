270499aef33f9205fd950fe42b4fabb39ec26b97
Laurens <s115911@ap.be>
2020-12-19 17:51:42 +0100

    added vectors

--- ShadowGame/ShadowGame/Shadow.cs
+++ ShadowGame/ShadowGame/Shadow.cs
@@ -10,8 +10,11 @@ namespace ShadowGame
 {
     public class Shadow : IGameObject
     {
-        Texture2D shadowTexture;
-        Animatie animatie;
+        private Texture2D shadowTexture;
+        private Animatie animatie;
+        private Vector2 positie;
+        private Vector2 snelheid;
+        private Vector2 versnelling;
      
         public Shadow(Texture2D texture)
         {
@@ -21,6 +24,11 @@ namespace ShadowGame
             {
                 animatie.AddFrame(new AnimationFrame(new Rectangle(i, 0, 126, 111)));
             }
+
+            positie = new Vector2(10, 10);
+            snelheid = new Vector2(1, 1); //10 lijkt een goede snelheid voor sprite
+            versnelling = new Vector2(0.1f, 0.1f);
+
             /*
             animatie.AddFrame(new AnimationFrame(new Rectangle(0, 0, 126, 111)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(126, 0, 126, 111)));
@@ -37,12 +45,41 @@ namespace ShadowGame
 
         public void Update(GameTime gameTime)
         {
+            Move();
             animatie.Update(gameTime);
+        }
+
+        private void Move() 
+        {
+            positie += snelheid;
+            snelheid += versnelling;
+
+            if (positie.X > 675 || positie.X < 0)
+            {
+                snelheid.X *= -1;
+                versnelling.X *= -1;
+            }
+            if (positie.Y > 375 || positie.Y < 0 )
+            {
+                snelheid.Y *= -1;
+                versnelling.Y *= -1;
+            }
+        }
+
+        private Vector2 Limit(Vector2 v, float max)
+        {
+            if (v.Length() > max)
+            {
+                var ratio = max / v.Length();
+                v.X *= ratio;
+                v.Y *= ratio;
+            }
+            return v;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(shadowTexture, new Vector2(10, 10), animatie.currentFrame.SourceRectangle, Color.White);
+            spriteBatch.Draw(shadowTexture, positie, animatie.currentFrame.SourceRectangle, Color.White);
         }
 
 
