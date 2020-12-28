5e182091d85732660d6d7b0c0263dd799958bf63
Laurens <s115911@ap.be>
2020-12-28 12:35:48 +0100

    trying to add level

---
+++ ShadowGame/ShadowGame/LevelDesign/LevelOne.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadowGame.LevelDesign
+{
+    class LevelOne
+    {
+        public Texture2D texture;
+
+        public byte[,] levelArray = new byte[,]
+        {
+            {0,0,0,0,0,0},
+            {0,0,0,0,0,0},
+            {1,0,1,0,1,0},
+            {0,1,0,1,0,1},
+        };
+
+        //private Block[,] blockArray = new Block[4, 6];
+
+    }
+}
