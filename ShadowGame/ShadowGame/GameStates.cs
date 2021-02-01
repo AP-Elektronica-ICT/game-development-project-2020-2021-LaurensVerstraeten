e557d5af41696640882865ee0f55a80c5a46ef3a
Laurens <s115911@ap.be>
2021-02-01 17:30:21 +0100

    implementing gamestates

---
+++ ShadowGame/ShadowGame/GameStates.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadowGame
+{
+    public abstract class GameStates
+    {
+        protected ContentManager _content;
+
+        protected GraphicsDevice _graphicsDevice;
+
+        protected Game1 _game;
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public GameStates(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            _game = game;
+
+            _graphicsDevice = graphicsDevice;
+
+            _content = content;
+        }
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        public abstract void Update(GameTime gameTime);
+
+    }
+}
---
+++ ShadowGame/ShadowGame/Menu/Menu.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadowGame.Menu
+{
+    public class Menu : GameStates
+    {
+        public Menu(Game1 game, GraphicsDevice graphicsDevice)
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
